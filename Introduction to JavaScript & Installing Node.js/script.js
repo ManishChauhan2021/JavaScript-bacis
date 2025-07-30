de33de0713e31b3956bb47e8d2339a56f6ce9eff
Manish Chauhan <chauhanmanish2021dev@gmail.com>
2025-07-30 20:16:46 +0530

    all code push

---
+++ Introduction to JavaScript & Installing Node.js/script.js
@@ -0,0 +1,18 @@
+ alert("Hello World")
+
+ console.log("code is runing...")
+ console.log("code is also runing...")
+ console.log("code is looking like a wow...")
+
+ var a = prompt("Enter your number")
+ var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+
+ if(isTrue){
+    console.log("computer is blasting")
+ }
+
+ else{
+     console.log("computer is not blasting")
+ }
+
+ console.log("your number is" + a)
---
+++ JavaScript Variables , Data Types & Objects/script.js
@@ -0,0 +1,42 @@
+console.log("Hey this is tutorial 2")
+
+let a = 5;
+a =a + 1;
+let b = 6;
+let c = "Manish Chauhan";
+let _a ="Harry";
+// var 55a = "Rohan"; Not Allowed
+
+// console.log(a + b + 8)
+// console.log(typeof a, typeof b, typeof c);
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+// const a1 = 6;
+// a1 = a1 + 1;  Not Allowed because a1 is constent
+
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r,)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q,typeof r,);
+
+
+let o = {
+    "name": "Harry",
+    "job code": 5640,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
