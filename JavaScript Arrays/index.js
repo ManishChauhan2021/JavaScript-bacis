fdedf35d7dc3a8b90d0858106d5d9a98076a8676
Manish Chauhan <chauhanmanish2021dev@gmail.com>
2025-08-06 15:47:15 +0530

    add arryas

---
+++ JavaScript Arrays/index.js
@@ -0,0 +1,23 @@
+// let arr = [1, 2, 4, 5, 7]
+// let arr2 = [3, 6, 8, 9,]
+// // index  0, 1, 2, 3, 4
+
+// arr[0] = 5666
+
+
+// console.log(arr, typeof arr);
+// console.log(arr.length)
+
+
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[4])
+
+// console.log(arr.toString());
+// console.log(arr.join(" and "));
+// console.log(arr.pop());
+// console.log(arr.push(100));
+// console.log(arr.shift())
+// console.log(arr.unshift("Jack"))
+// console.log(arr.concat(arr2))
+// console.log(arr, arr2.sort())
